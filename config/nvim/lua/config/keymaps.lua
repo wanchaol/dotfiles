ce9b6a64c509d7e863e91142eb4ce692bd1e65ff
Wanchao Liang <wanchaol@users.noreply.github.com>
2025-06-26 15:41:16 -0700

    update

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -4,3 +4,11 @@
 
 vim.keymap.set({ "n", "v" }, "<leader>aa", "<cmd>AvanteAsk<cr>", { desc = "ask" })
 vim.keymap.set({ "n", "v" }, "<leader>ar", "<cmd>AvanteAsk<cr>", { desc = "refresh" })
+
+-- to disable the nasty <esc> + jk dragging lines issue
+vim.api.nvim_del_keymap("n", "<M-k>")
+vim.api.nvim_del_keymap("n", "<M-j>")
+vim.api.nvim_del_keymap("v", "<M-k>")
+vim.api.nvim_del_keymap("v", "<M-j>")
+vim.api.nvim_del_keymap("i", "<M-k>")
+vim.api.nvim_del_keymap("i", "<M-j>")
